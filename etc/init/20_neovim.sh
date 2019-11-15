392928a419b98fbab297493eb350bd0fecaad227
borley1211 <km.isetan@gmail.com>
2019-11-15 19:04:44 +0900

    FIX: around nvim

--- etc/init/20_neovim.sh
+++ etc/init/20_neovim.sh
@@ -4,9 +4,9 @@ echo "** RUNNING $(basename $0) **"
 
 if ! type "nvim" "nvim.appimage" &>/dev/null; then
     mkdir -p "${HOME}/.local/bin"
-    curl -o "$HOME/.local/bin/nvim.appimage" -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-    chmod u+x $HOME/.local/bin/nvim.appimage
-    echo "Installed nvim.appimage!"
+    curl -o "$HOME/.local/bin/nvim" -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+    chmod u+x $HOME/.local/bin/nvim
+    echo "Installed nvim(as appimage)!"
 else
     echo "You MUST install neovim manually."
 fi
