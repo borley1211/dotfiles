c060fc5e005989b7af4fa8915f3d04e20c2f05c1
km <47778507+borley1211@users.noreply.github.com>
2019-11-20 12:02:04 +0900

    Update 20_zsh.sh

--- etc/init/20_zsh.sh
+++ etc/init/20_zsh.sh
@@ -1,13 +1,6 @@
 #!bash
-source ../install
-echo ""
-logging "INFO" "** RUNNING $(basename $0) **"
-
 if can_use_sudo; then
     sudo apt-get install zsh chsh -y
 else
     apt-get install zsh chsh -y
 fi
-
-logging "SUCCESS" "** FINISHED **"
-exit 0
