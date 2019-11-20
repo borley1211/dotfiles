a5438e70abbfc903fb93bd473de0b25b72a39a0b
km <47778507+borley1211@users.noreply.github.com>
2019-11-20 11:37:08 +0900

    Update 10_pyenv.sh

--- etc/init_lazy/10_pyenv.sh
+++ etc/init_lazy/10_pyenv.sh
@@ -5,14 +5,13 @@ echo "** RUNNING $(basename $0) **"
 
 # install pyenv and plugins
 if [ ! -d "$HOME/.pyenv" ]; then
-    git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+    curl https://pyenv.run | bash
 else
     echo "pyenv is already exists."
 fi
-eval "$(pyenv init -)"
+exec ${SHELL}
 
 # install pyenv-ccache
-#sudo apt -y install ccache
 if [ ! -e "$(pyenv root)/plugins/pyenv-ccache" ]; then
     git clone https://github.com/yyuu/pyenv-ccache.git $(pyenv root)/plugins/pyenv-ccache
 fi
@@ -21,9 +20,6 @@ fi
 if [ ! -e "$(pyenv root)/plugins/pyenv-pip-migrate" ]; then
     git clone git://github.com/pyenv/pyenv-pip-migrate.git $(pyenv root)/plugins/pyenv-pip-migrate
 fi
-
-# install dependencies for build python
-#sudo apt -y install 
 
 echo "** FINISHED **"
 exit 0
