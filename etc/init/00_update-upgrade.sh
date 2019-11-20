faeae7f3a9b5bc5d94365f9096ebd4261eced42d
km <47778507+borley1211@users.noreply.github.com>
2019-11-20 12:41:29 +0900

    Update 00_update-upgrade.sh

--- etc/init/00_update-upgrade.sh
+++ etc/init/00_update-upgrade.sh
@@ -1,8 +1,14 @@
 #!bash
-if can_use_sudo; then
-    sudo apt-get update
-    sudo apt-get upgrade -y
-else
-    apt-get update
-    apt-get upgrade -y
-fi
+source ../install
+
+update_and_upgrade() {
+    if can_use_sudo; then
+        sudo apt-get update
+        sudo apt-get upgrade -y
+    else
+        apt-get update
+        apt-get upgrade -y
+    fi
+}
+
+logexec update_and_upgrade
