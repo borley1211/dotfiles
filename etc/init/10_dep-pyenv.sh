5371aabaf2a077dc3252272466aa0aeefd4eccee
km <47778507+borley1211@users.noreply.github.com>
2019-11-20 11:57:34 +0900

    Update 10_dep-pyenv.sh

--- etc/init/10_dep-pyenv.sh
+++ etc/init/10_dep-pyenv.sh
@@ -1,5 +1,7 @@
 #!bash
-. "../install"
+source "../install"
+echo ""
+logging "INFO" "** RUNNING $(basename $0) **"
 
 # for pyenv
 DEPENDENCIES=("gcc" "libssl-dev" "libbz2-dev" "libreadline-dev" "libsqlite3-dev" "zlib1g-dev")
@@ -8,3 +10,10 @@ DEPENDENCIES=("gcc" "libssl-dev" "libbz2-dev" "libreadline-dev" "libsqlite3-dev"
 DEPENDENCIES=("${DEPENDENCIES}" "ccache")
 
 if can_use_sudo; then
+    sudo apt-get install "${DEPENDENCIES[@]}" -y
+else
+    apt-get install "${DEPENDENCIES[@]}" -y
+fi
+
+logging "SUCCESS" "** FINISHED **"
+exit 0
