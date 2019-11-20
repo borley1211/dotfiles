366b4dcbfb27be428c6de2d8a65fcea555764312
km <47778507+borley1211@users.noreply.github.com>
2019-11-20 12:33:59 +0900

    Update 99_prezto.sh

--- etc/init_lazy/99_prezto.sh
+++ etc/init_lazy/99_prezto.sh
@@ -1,18 +1,18 @@
 #!bash
-echo ""
-echo "** RUNNING $(basename $0) **"
+source ../install
 
-if [ ! "$(command -v zsh)" == "1" ]  # if zsh exists
-then
-    # echo "zsh exists!"
-    if [ ! -d ${ZDOTDIR:-$HOME}/.zprezto ]; then
-        zsh -c "git clone --recursive https://github.com/sorin-ionescu/prezto.git ${ZDOTDIR:-$HOME}/.zprezto && setopt EXTENDED_GLOB; for rcfile in ${ZDOTDIR:-$HOME}/.zprezto/runcoms/^README.md(.N); do; ln -sv $rcfile ${ZDOTDIR:-$HOME}/.${rcfile:t}; done"
+init_prezto() {
+    if [ ! "$(command -v zsh)" == "1" ]  # if zsh exists
+    then
+        # echo "zsh exists!"
+        if [ ! -d ${ZDOTDIR:-$HOME}/.zprezto ]; then
+            zsh -c "git clone --recursive https://github.com/sorin-ionescu/prezto.git ${ZDOTDIR:-$HOME}/.zprezto && setopt EXTENDED_GLOB; for rcfile in ${ZDOTDIR:-$HOME}/.zprezto/runcoms/^README.md(.N); do; ln -sv $rcfile ${ZDOTDIR:-$HOME}/.${rcfile:t}; done"
+        else
+            echo "NOTICE: Prezto is already exists."
+        fi
     else
-        echo "NOTICE: Prezto is already exists."
+        echo "ERROR: You MUST run me again, after installing ZSH."
     fi
-else
-    echo "ERROR: You MUST run me again, after installing ZSH."
-fi
+}
 
-echo "** FINISHED **"
-exit 0
+logexec init_prezto
