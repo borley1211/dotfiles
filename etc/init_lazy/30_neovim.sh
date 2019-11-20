dd0520c01d73f50310782b130ab6d554fcdcc9bb
km <47778507+borley1211@users.noreply.github.com>
2019-11-20 12:49:45 +0900

    Update 30_neovim.sh

--- etc/init_lazy/30_neovim.sh
+++ etc/init_lazy/30_neovim.sh
@@ -1,17 +1,21 @@
 #!bash
 source ../install
 
-if ! type "nvim" "nvim.appimage" &>/dev/null; then
-    if is_linux; then
-        mkdir -p "${HOME}/.local/bin"
-        curl -o "${HOME}/.local/bin/nvim" -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-        chmod u+x ${HOME}/.local/bin/nvim
-        log_pass "Installed nvim(as appimage)!"
-    else # is_mac
-        brew install neovim
-        log_pass "Installed nvim!"
+install_neovim() {
+    if ! type "nvim" "nvim.appimage" &>/dev/null; then
+        if is_linux; then
+            mkdir -p "${HOME}/.local/bin"
+            curl -o "${HOME}/.local/bin/nvim" -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+            chmod u+x ${HOME}/.local/bin/nvim
+            log_pass "Installed nvim(as appimage)!"
+        else # is_mac
+            brew install neovim
+            log_pass "Installed nvim!"
+        fi
+    else
+        log_info "Have you already installed NeoVim?"
+        log_info "If NO, You MUST install NeoVim manually."
     fi
-else
-    log_info "Have you already installed NeoVim?"
-    log_info "If NO, You MUST install NeoVim manually."
-fi
+}
+
+logexec install_neovim
