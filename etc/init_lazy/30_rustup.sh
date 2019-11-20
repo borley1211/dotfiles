5b91f125534405444b373caf7878d8188c9919b7
km <47778507+borley1211@users.noreply.github.com>
2019-11-20 11:27:50 +0900

    Update 30_rustup.sh

--- etc/init_lazy/30_rustup.sh
+++ etc/init_lazy/30_rustup.sh
@@ -1,2 +1,14 @@
 #!bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+exec ${SHELL}
+
+COMPLETIONSPATH="~/.local/share/bash-completion/completions"
+mkdir -p ${COMPLETIONSPATH}
+rustup completions bash > ${COMPLETIONSPATH}/rustup
+rustup completions cargo bash > ${COMPLETIONSPATH}/cargo
+
+mkdir -p ~/.zfunc
+rustup completions zsh > ~/.zfunc/_rustup
+rustup completions cargo zsh > ~/.zfunc/_cargo
+
+exit 0
