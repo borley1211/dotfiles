72b99b34bd393d2ea4ed560ad4eeedcd4f07a325
km <47778507+borley1211@users.noreply.github.com>
2019-11-20 12:01:40 +0900

    Update 10_dep-pyenv.sh

--- etc/init/10_dep-pyenv.sh
+++ etc/init/10_dep-pyenv.sh
@@ -1,8 +1,4 @@
 #!bash
-source "../install"
-echo ""
-logging "INFO" "** RUNNING $(basename $0) **"
-
 # for pyenv
 DEPENDENCIES=("gcc" "libssl-dev" "libbz2-dev" "libreadline-dev" "libsqlite3-dev" "zlib1g-dev")
 
@@ -14,6 +10,3 @@ if can_use_sudo; then
 else
     apt-get install "${DEPENDENCIES[@]}" -y
 fi
-
-logging "SUCCESS" "** FINISHED **"
-exit 0
