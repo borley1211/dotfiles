8eada59ed4eaeca129edd3b0b6a8e9e1c16f77d9
km <47778507+borley1211@users.noreply.github.com>
2019-11-20 12:01:09 +0900

    Update 00_update-upgrade.sh

--- etc/init/00_update-upgrade.sh
+++ etc/init/00_update-upgrade.sh
@@ -1,8 +1,4 @@
 #!bash
-source ../install
-echo ""
-logging "INFO" "** RUNNING $(basename $0) **"
-
 if can_use_sudo; then
     sudo apt-get update
     sudo apt-get upgrade -y
@@ -10,6 +6,3 @@ else
     apt-get update
     apt-get upgrade -y
 fi
-
-logging "SUCCESS" "** FINISHED **"
-exit 0
