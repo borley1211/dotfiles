458c769ce84f37b50f1bc399bb927aa9719e462e
km <47778507+borley1211@users.noreply.github.com>
2019-11-20 12:56:19 +0900

    Create 10_ssh.sh

---
+++ etc/init_sys/10_ssh.sh
@@ -0,0 +1,16 @@
+#!bash
+source ../install
+
+init_ssh() {
+    if can_use_sudo; then
+        sudo apt-get install ssh openssh-server -y
+        sudo systemctl daemon-reload
+        sudo systemctl enable --now sshd.service
+    else
+        apt-get install ssh openssh-server -y
+        systemctl daemon-reload
+        systemctl enable --now sshd.service
+    fi
+}
+
+logexec init_ssh
