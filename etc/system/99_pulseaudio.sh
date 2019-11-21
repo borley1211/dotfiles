51b22bda4bb0be070d6d066f4d53ddc2e091beab
borley1211 <km.isetan@gmail.com>
2019-11-21 12:54:25 +0900

    ADD: package.json for npm, FIX: init_pulse

--- etc/system/99_pulseaudio.sh
+++ etc/system/99_pulseaudio.sh
@@ -6,14 +6,13 @@ init_pulse() {
     if type "snap" >/dev/null; then
         sudo snap install pulseaudio
         sudo snap connect pulseaudio:home core
-    else
-        sudo apt-get -o APT::Install-Suggests=true install pulseaudio -y
-        sudo gpasswd -a ${USER} audio
-        sudo gpasswd -a ${USER} pulse
-        sudo gpasswd -a ${USER} pulse-access
-        sudo systemctl daemon-reload
-        systemctl enable --now pulseaudio --user
     fi
+    sudo apt-get -o APT::Install-Suggests=true install pulseaudio -y
+    sudo gpasswd -a ${USER} audio
+    sudo gpasswd -a ${USER} pulse
+    sudo gpasswd -a ${USER} pulse-access
+    sudo systemctl daemon-reload
+    systemctl enable --now pulseaudio --user
 }
 
 logexec init_pulse
