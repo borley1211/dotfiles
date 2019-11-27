dee5f1b2ddbe3a42e3ba7d1a5f61d23cfd1e2cc6
km <47778507+borley1211@users.noreply.github.com>
2019-11-28 07:53:50 +0900

    Update 40_npm_by_n.sh

--- etc/init/40_npm_by_n.sh
+++ etc/init/40_npm_by_n.sh
@@ -4,7 +4,6 @@ source ${DOTPATH:-~/Dotfiles}/etc/install
 init_npm_by_n() {
     if ! type "n" >/dev/null; then
         curl -L https://git.io/n-install | bash
-        exec ${SHELL:-/usr/bin/bash}
     else
         log_info "It seems you've already installed n and npm. Exit."
     fi
