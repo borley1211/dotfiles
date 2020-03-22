ab155aff73c8b14ed2482620fdd0e08b411f865e
borley1211 <km.isetan@gmail.com>
2020-03-22 20:02:56 +0900

    Update rc.sh

--- rc.sh
+++ rc.sh
@@ -109,7 +109,7 @@ export DOTPATH="${HOME}/Dotfiles"
 alias dotutil="make -C ${DOTPATH}"
 
 # - goenv
-if [ -e "${HOME}/.goenv" ] && ! [ -e "${HOME}/.anyenv"] ; then
+if [ -e "${HOME}/.goenv" ] && ! [ -e "${HOME}/.anyenv" ] ; then
     export GOENV_ROOT="$HOME/.goenv"
     export PATH="$GOENV_ROOT/bin:$PATH"
     eval "$(goenv init -)"
