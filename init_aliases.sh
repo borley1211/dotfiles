b128f773bb920a3babd8e14758b1969fb5638ded
borley1211 <km.isetan@gmail.com>
2020-04-11 11:51:34 +0900

    FIX: init_aliases.sh

--- init_aliases.sh
+++ init_aliases.sh
@@ -1,4 +1,7 @@
+# shellcheck disable=SC2139
 alias relogin="exec $SHELL -l"
 alias reshell="exec $SHELL"
+
 alias inodecheck='echo "==== `pwd` ====" ; for i in `ls -A1` ; do echo -n "## $i ### " ; echo "(`find ./$i -type f | wc -l`)" ; done | sort -nr'
+
 alias gogh='bash -c  "$(wget -qO- https://git.io/vQgMr)"'
