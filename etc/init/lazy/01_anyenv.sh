df6397142503fff4c52d81c2b645bc579e4a9c9a
km <47778507+borley1211@users.noreply.github.com>
2020-04-12 20:39:50 -0700

    Update 01_anyenv.sh

--- etc/init/lazy/01_anyenv.sh
+++ etc/init/lazy/01_anyenv.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-source "${DOTPATH:-~/Dotfiles}/etc/install"
+git clone https://github.com/anyenv/anyenv ~/.anyenv
+export PATH="$HOME/.anyenv/bin:$PATH"
+anyenv init
+anyenv install --init
 
-function init_anyenv() {
-    git clone https://github.com/anyenv/anyenv ~/.anyenv
-    export PATH="$HOME/.anyenv/bin:$PATH"
-    anyenv init
-    anyenv install --init
+mkdir -p $(anyenv root)/plugins
+git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
 
-    mkdir -p $(anyenv root)/plugins
-    git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
-
-    anyenv install goenv
-    anyenv install rbenv
-    anyenv install nodenv
-}
-
-logexec init_anyenv
+anyenv install goenv
+anyenv install rbenv
+#anyenv install nodenv
