4e6c46438392e45697df0dab44706da30509e342
km <47778507+borley1211@users.noreply.github.com>
2020-04-12 20:40:57 -0700

    Update 10_pyenv.sh

--- etc/init/lazy/10_pyenv.sh
+++ etc/init/lazy/10_pyenv.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-source ${DOTPATH:-~/Dotfiles}/etc/install
+# install pyenv and plugins
+if [ ! -d "$HOME/.pyenv" ]; then
+    curl https://pyenv.run | bash
+else
+    echo "pyenv is already exists."
+fi
+export PATH="/home/borley/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
-init_pyenv() {
-    # install pyenv and plugins
-    if [ ! -d "$HOME/.pyenv" ]; then
-        curl https://pyenv.run | bash
-    else
-        echo "pyenv is already exists."
-    fi
-    export PATH="/home/borley/.pyenv/bin:$PATH"
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
+# install pyenv-ccache
+if [ ! -e "$(pyenv root)/plugins/pyenv-ccache" ]; then
+    git clone https://github.com/yyuu/pyenv-ccache.git $(pyenv root)/plugins/pyenv-ccache
+fi
 
-    # install pyenv-ccache
-    if [ ! -e "$(pyenv root)/plugins/pyenv-ccache" ]; then
-        git clone https://github.com/yyuu/pyenv-ccache.git $(pyenv root)/plugins/pyenv-ccache
-    fi
-
-    # install pyenv-pip-migrate
-    if [ ! -e "$(pyenv root)/plugins/pyenv-pip-migrate" ]; then
-        git clone git://github.com/pyenv/pyenv-pip-migrate.git $(pyenv root)/plugins/pyenv-pip-migrate
-    fi
-}
-
-logexec init_pyenv
+# install pyenv-pip-migrate
+if [ ! -e "$(pyenv root)/plugins/pyenv-pip-migrate" ]; then
+    git clone git://github.com/pyenv/pyenv-pip-migrate.git $(pyenv root)/plugins/pyenv-pip-migrate
+fi
