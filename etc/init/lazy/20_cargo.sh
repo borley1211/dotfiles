18f7537b79b2e7965ec33a757f1c54079b2f052d
km <47778507+borley1211@users.noreply.github.com>
2020-04-12 20:39:02 -0700

    Update 20_cargo.sh

--- etc/init/lazy/20_cargo.sh
+++ etc/init/lazy/20_cargo.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-source ${DOTPATH:-~/Dotfiles}/etc/install
-
-init_cargo_pkgs() {
-    source ~/.cargo/env
-    cargo install cargo-update topgrade
-    cargo install starship && ( echo 'eval $( starship init zsh )' >> "$HOME/.zshrc" )
-}
-
-logexec init_cargo_pkgs || exit
+source ~/.cargo/env
+sudo apt install pkg-config -y
+cargo install cargo-update topgrade
+cargo install starship && ( echo 'eval $( starship init zsh )' >> "$HOME/.zshrc" )
