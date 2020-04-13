c6e170749f56f0b974822129e529b0c46ee4b7dd
km <47778507+borley1211@users.noreply.github.com>
2020-04-12 20:42:18 -0700

    Update 40_fzf.sh

--- etc/init/lazy/40_fzf.sh
+++ etc/init/lazy/40_fzf.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-source ${DOTPATH:-~/Dotfiles}/etc/install
-
-init_fzf() {
-    if [ ! -d "$HOME/.fzf" ]; then
-        git clone https://github.com/junegunn/fzf.git ~/.fzf
-        ~/.fzf/install
-    else
-        echo "fzf is already exists."
-    fi
-}
-
-logexec init_fzf
+if [ ! -d "$HOME/.fzf" ]; then
+    git clone https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+else
+    echo "fzf is already exists."
+fi
