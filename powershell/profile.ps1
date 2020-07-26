273bc2af578f114ab82d647082b1794db7610066
km <47778507+borley1211@users.noreply.github.com>
2020-07-27 03:19:07 +0900

    Update profile.ps1

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,14 +1,4 @@
-﻿Set-Alias ls "$HOME\scoop\shims\ls.exe"
-Set-Alias ln "$HOME\scoop\shims\ln.exe"
-Set-Alias mkdir "$HOME\scoop\shims\mkdir.exe"
-
-switch ($PSEdition) {
-    "Core" {$PSNAME = "pwsh"}
-    Default {$PSNAME = "powershell"}
-}
-
-function reshell {
-    Invoke-Expression "${PSNAME} -NoLogo"
-}
+# enable completion in current shell, use absolute path because PowerShell Core not respect $env:PSModulePath
+Import-Module "$($(Get-Item $(Get-Command scoop).Path).Directory.Parent.FullName)\modules\scoop-completion"
 
 Invoke-Expression (&starship init powershell)
