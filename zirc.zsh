8b6f3b4980bc48f39e1d995ecca32967c51f50ef
borley1211 <km.isetan@gmail.com>
2022-02-18 23:18:04 +0900

    feat(ZI - zsh plugin manager): :sparkles: zinit -> ZI

---
+++ zirc.zsh
@@ -0,0 +1,51 @@
+# Zinit config
+
+typeset -g ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=8"
+
+# - COMPLETIONS
+
+# - HIGHLIGHTING
+zi load zsh-users/zsh-syntax-highlighting
+# syntax highlighting with dircolors
+zi load trapd00r/zsh-syntax-highlighting-filetypes
+# check url
+zi light ascii-soup/zsh-url-highlighter
+# generate color from $LS_COLORS
+# zi light marlonrichert/zcolors
+
+# - HISTORY
+zi light jimhester/per-directory-history
+#  mask secret for history
+zi light jgogstad/passwordless-history
+
+# - COMPLETIONS
+# default completions
+zi light zsh-users/zsh-autosuggestions
+zi light zsh-users/zsh-completions
+# for npm
+zi light lukechilds/zsh-better-npm-completion
+# for anyenv
+#zi light ress997/zsh-completions-anyenv
+
+# - SEARCH
+# fzf
+zi ice from"gh-r" as"program"
+zi load junegunn/fzf
+# fzf-history-search
+zi ice lucid wait'0'
+zi light joshskidmore/zsh-fzf-history-search
+# anyframe - fuzzy history search
+# zi light mollifier/anyframe
+
+# - OTHERS
+# interactive-cd
+zi ice as"plugin"
+zi light b4b4r07/enhancd
+# dotofiles manager
+zi light ssh0/dot
+# 略語登録/展開
+zi load momo-lab/zsh-abbrev-alias
+
+# - RELOAD_COMPLETIONS
+zi cdclear -q >/dev/null
+zi compinit >/dev/null
