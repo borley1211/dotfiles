eb4c9ee26e28355da062ff58a5698ad5b0226426
Borley <47778507+borley1211@users.noreply.github.com>
2022-07-12 23:36:18 +0900

    chore: :adhesive_bandage: add "aqua i" in aqua.sh

--- etc/init/lazy/00_aqua.sh
+++ etc/init/lazy/00_aqua.sh
@@ -8,6 +8,11 @@ if [ ! -d "$HOME/.local/bin/aqua" ]; then
     curl -sSfL \
     https://raw.githubusercontent.com/aquaproj/aqua-installer/v0.6.0/aqua-installer |
     bash -s -- -i ~/.local/bin/aqua
+
+    aqua i
+
+    # add completions (can't yet)
+    # echo "asdf completion" >> "${HOME}/.zfunc/_asdf"
 else
     echo "aqua is already exists."
 fi
