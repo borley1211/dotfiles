015aae85c68c1cfab6983aa17efcb7d7b307b21b
borley1211 <km.isetan@gmail.com>
2023-08-21 04:35:21 +0900

    style(update tools): :art: remove duplicate part from zirc.zsh

--- zirc.zsh
+++ zirc.zsh
@@ -6,8 +6,6 @@ typeset -A ZI
 ZI[BIN_DIR]="${HOME}/.zi/bin"
 source "${ZI[BIN_DIR]}/zi.zsh"
 
-# - COMPLETIONS
-
 # - HIGHLIGHTING
 zi load zsh-users/zsh-syntax-highlighting
 # syntax highlighting with dircolors
