b5aa1c69113b0f60d8e937ac45dcf8a421e8ca04
borley1211 <km.isetan@gmail.com>
2024-04-03 17:55:35 +0900

    chore(rename file): :truck: .env -> setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,10 @@
+# shellcheck disable=SC2148
+# Global ENVVAR settings
+
+# - Encoding
+export LANG="ja_JP.UTF-8"
+export LC_ALL="ja_JP.UTF-8"
+
+# - LunarVim
+export EDITOR="lvim"
+export VISUAL=${EDITOR}
