5c1ed643be94ece5ceb5d7653cf77cb803f599ef
borley1211 <km.isetan@gmail.com>
2025-07-03 15:56:02 +0900

    Add indent-blankline to lvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -2,6 +2,12 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 lvim.colorscheme = "monokai.nvim"
+lvim.plugins = {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    enabled = false,
+  }
+}
 
 vim.opt.wrap = true
 vim.opt.linespace = 4
