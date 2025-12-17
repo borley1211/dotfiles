6ad7a1d7834f12aef8f56ef35381f2bd36e95ac6
borley1211 <km.isetan@gmail.com>
2025-12-18 01:08:40 +0900

    Add destyle-ansi function to init_aliases.sh for trimming ANSI control sequences

--- init_aliases.sh
+++ init_aliases.sh
@@ -9,3 +9,16 @@ function gogh() {
 # gogh
 
 alias edit='$EDITOR'
+
+function destyle-ansi() {
+    # destyle-ansi - Trim ANSI Control Sequence from text
+    #   Source - https://stackoverflow.com/a/30938702
+    #   Posted by sandy_1111, modified by community. See post 'Timeline' for change history
+    #   Retrieved 2025-12-05, License - CC BY-SA 3.0
+
+    sed -r \
+        -e 's/\x1B\[[0-9;?]*[ -\/]*[@-~]//g' \
+        -e 's/\x1B\][^\x1B]*\x1B\\//g' \
+        -e 's/\x1B[P^_][^\x1B]*\x1B\\//g' \
+        "$@"
+}
